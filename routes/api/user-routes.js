dd70fd0534e1dfcb045b3fce690eee02c58f46e1
ThereWolf <bdunham484@gmail.com>
2022-08-01 10:37:27 -0500

    update

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,7 +52,7 @@ router.get('/:id', (req, res) => {
         })
         .catch(err => {
             console.log(err);
-            res.status(500).jsone(err);
+            res.status(500).json(err);
         });
 });
 
@@ -65,11 +65,11 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 
