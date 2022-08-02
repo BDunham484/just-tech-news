d9aff7bd5e5eeded1bbd6b6a90ea217e1ff96070
ThereWolf <bdunham484@gmail.com>
2022-08-02 15:11:30 -0500

    add comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,7 +7,7 @@ router.get('/', (req, res) => {
     Post.findAll({
         //query configuration
         attributes: ['id', 'post_url', 'title', 'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         order: [['created_at', 'DESC']],
         include: [
@@ -27,21 +27,24 @@ router.get('/', (req, res) => {
                 }
             },
             {
-                model: User, 
+                model: User,
                 attrubutes: ['username']
             }
         ]
     })
-    .then(dbPostData => {
-        // console.log(dbPostData[0].get({plain: true}));
-        //pass a single post object into the homepage template
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
-    })
-    .catch(err => {
-        console.log(err);
-        req.statusCode(500).json(err);
-    });
+        .then(dbPostData => {
+            // console.log(dbPostData[0].get({plain: true}));
+            //pass a single post object into the homepage template
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', { 
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            req.statusCode(500).json(err);
+        });
 });
 
 router.get('/login', (req, res) => {
@@ -51,5 +54,48 @@ router.get('/login', (req, res) => {
     }
     res.render('login');
 })
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'post_url', 'title', 'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            //inclued the comment model here
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                inclued: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User, 
+                attrubutes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        //serialize the data
+        const post = dbPostData.get({ plain: true});
+        //pass data to templat
+        res.render('single-post', { 
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
